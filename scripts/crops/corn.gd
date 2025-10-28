c11b65a5d68b2e49cf4e3ff15059b9864b1a4c8c
Ichiroki <fahrezirizqiawan12649@gmail.com>
2025-10-28 17:19:13 +0700

    add Corn Seed Item, organize much files

--- scripts/crops/corn.gd
+++ scripts/crops/corn.gd
@@ -2,26 +2,16 @@ extends "crop.gd"
 
 @export var crop_item: ItemsData = preload("res://core/resources/items/vegetable_fruit/corn_item.tres")
 
-func _ready():
-	await get_tree().process_frame
-	$Name.text = crop_item.item_name
-	$Time.text = str(times)
-	times = $Timer.time_left
-	
-func _process(delta: float) -> void:
-	if times > 0:
-		times -= delta
-		$Time.text = str(round(times))
-	else:
-		times = 0
-		set_process(false)
-	
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.has_method("player"):
-		$Name.visible = true
-		$Time.visible = true
+@onready var name_label: Label = $Name
+@onready var time_label: Label = $Time
 
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.has_method("player"):
-		$Name.visible = false
-		$Time.visible = false
+func _ready():
+	name_label.text = crop_item.item_name
+	set_process(true)
+
+func _process(delta: float) -> void:
+	time_label.visible = true
+	if not is_ready:
+		time_label.text = str(round(time_left))
+	else:
+		time_label.text = "Ready!"
